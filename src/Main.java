4276ac4736e1130381141bedfe485c649f8d58e0
Bhavana <bhavanapajjuri2017@gmail.com>
2017-09-16 21:19:00 +0530

    Add files via upload
    throwing Exception in setStudents method

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,10 @@ public class Main {
 		//You may test that your code works find here
 		//Please check that your code works and has no 
 		//compilation problems before to submit
+		
+		StudentArrayOperation studentList = new StudentGroup(5);
+		studentList.setStudents(null);
+		System.out.println("bhavana");
 	}
 
 }
